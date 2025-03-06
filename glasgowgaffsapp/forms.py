42139a8ff492d94760ff010743e1396f379cbc93
“Robert <2901941b@student.gla.ac.uk>
2025-03-06 20:00:51 +0000

    SignUp and Login styling

--- glasgowgaffsapp/forms.py
+++ glasgowgaffsapp/forms.py
@@ -2,7 +2,14 @@ from django import forms
 from django.contrib.auth.models import User
 
 class UserForm(forms.ModelForm):
-    password = forms.CharField(widget=forms.PasswordInput())
+    password = forms.CharField(
+        widget=forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Password'})
+    )
+
     class Meta:
         model = User
-        fields = ('username', 'email', 'password',)
+        fields = ('username', 'email', 'password')
+        widgets = {
+            'username': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Username'}),
+            'email': forms.EmailInput(attrs={'class': 'form-control', 'placeholder': 'E-mail'}),
+        }
