bcad47092f1d667f52cf7f86ab677737e02fffee
“Robert <2901941b@student.gla.ac.uk>
2025-03-07 11:17:25 +0000

    Event-Location correlation + population script

--- glasgowgaffsapp/models.py
+++ glasgowgaffsapp/models.py
@@ -6,7 +6,6 @@ class Location(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     address = models.CharField(max_length=200)
     name = models.CharField(max_length=50)
-    address = models.CharField(max_length=200)
 
     def __str__(self):
         return self.name
@@ -14,12 +13,11 @@ class Location(models.Model):
 class Event(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     title = models.CharField(max_length=50)
-    description = models.CharField(max_length=200)
+    description = models.TextField(max_length=200)
     date = models.DateField()
     time = models.TimeField()
-    # location = models.ForeignKey(Location, on_delete=models.CASCADE)
-    # attendees = models.ManyToManyField(User, related_name="registered_events", blank=True)
-    # created_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name="created_events")
+    location = models.ForeignKey(Location, on_delete=models.CASCADE, default="Student Accommodation")
+    
 
     def __str__(self):
         return self.title
